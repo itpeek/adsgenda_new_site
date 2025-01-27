69bac9425b2e091073e26ee6c7c901f74c7915f4
adsgenda <poseidon_zz@outlook.co.th>
2025-01-27 17:46:43 +0700

    Update logoscarousel.js

--- src/app/components/logoscarousel.js
+++ src/app/components/logoscarousel.js
@@ -13,6 +13,7 @@ const LogosCarousel = () => {
     <div
       className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
     >
+        <div className='w-full max-w-5xl mx-auto px-4 md:px-6 py-24'>
       <ul
         ref={logosRef}
         className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
@@ -42,6 +43,7 @@ const LogosCarousel = () => {
           <img src="./sass.svg" alt="Sass" />
         </li>
       </ul>
+    </div>
     </div>
   );
 };
