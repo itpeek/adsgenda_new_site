0f29893dd46b01cfcd2914990884878f9a22d905
adsgenda <poseidon_zz@outlook.co.th>
2025-01-27 18:00:18 +0700

    Update logoscarousel.js

--- src/app/components/logoscarousel.js
+++ src/app/components/logoscarousel.js
@@ -1,4 +1,6 @@
 import { useEffect, useRef } from 'react';
+import { customerData } from "../data/data";
+import Image from "next/image";
 
 const LogosCarousel = () => {
   const logosRef = useRef(null);
@@ -17,30 +19,13 @@ const LogosCarousel = () => {
         ref={logosRef}
         className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
       >
-        <li>
-          <img src="./facebook.svg" alt="Facebook" />
-        </li>
-        <li>
-          <img src="./disney.svg" alt="Disney" />
-        </li>
-        <li>
-          <img src="./airbnb.svg" alt="Airbnb" />
-        </li>
-        <li>
-          <img src="./apple.svg" alt="Apple" />
-        </li>
-        <li>
-          <img src="./spark.svg" alt="Spark" />
-        </li>
-        <li>
-          <img src="./samsung.svg" alt="Samsung" />
-        </li>
-        <li>
-          <img src="./quora.svg" alt="Quora" />
-        </li>
-        <li>
-          <img src="./sass.svg" alt="Sass" />
-        </li>
+        {customerData.map((item, index)=>{
+            return(
+                <li>
+                <Image src={item.image} key={index} alt={index + 1}/>
+                </li>
+            )
+        })}
       </ul>
     </div>
   );
