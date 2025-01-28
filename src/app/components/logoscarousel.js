a1cf92bbe81a682204fc68e32bac798ed5ca38ef
adsgenda <poseidon_zz@outlook.co.th>
2025-01-28 13:58:31 +0700

    Update logoscarousel.js

--- src/app/components/logoscarousel.js
+++ src/app/components/logoscarousel.js
@@ -1,12 +1,10 @@
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { customerData } from "../data/data";
 import Image from "next/image";
 
 const LogosCarousel = () => {
-  const [isPaused, setIsPaused] = useState(false);
   const logoRef = useRef(null);
 
-  // Manage the animation based on the hover/click state
   useEffect(() => {
     const ul = logoRef.current;
     if (ul) {
@@ -15,28 +13,13 @@ const LogosCarousel = () => {
     }
   }, []);
 
-  const handleMouseEnter = () => {
-    setIsPaused(true); // Pause animation on hover
-  };
-
-  const handleMouseLeave = () => {
-    setIsPaused(false); // Resume animation when hover is removed
-  };
-
-  const handleClick = () => {
-    setIsPaused(true); // Pause animation on click
-  };
-
   return (
     <div
       className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
-      onMouseEnter={handleMouseEnter} 
-      onMouseLeave={handleMouseLeave}
-      onClick={handleClick}
     >
       <ul
         ref={logoRef}
-        className={`flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none ${isPaused ? 'pause-animation' : 'animate-infinite-scroll'}`}
+        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
       >
         {customerData.map((item, index) => {
           return (
