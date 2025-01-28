24743a8d91b737234d22f6bbe8975efa69e649de
adsgenda <poseidon_zz@outlook.co.th>
2025-01-28 11:27:41 +0700

    Update logoscarousel.js

--- src/app/components/logoscarousel.js
+++ src/app/components/logoscarousel.js
@@ -3,20 +3,12 @@ import { customerData } from "../data/data";
 import Image from "next/image";
 
 const LogosCarousel = () => {
-  const logosRef = useRef(null);
-
-  useEffect(() => {
-    const ul = logosRef.current;
-    ul.insertAdjacentHTML('afterend', ul.outerHTML);
-    ul.nextSibling.setAttribute('aria-hidden', 'true');
-  }, []);
 
   return (
     <div
       className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
     >
       <ul
-        ref={logosRef}
         className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
       >
         {customerData.map((item, index)=>{
