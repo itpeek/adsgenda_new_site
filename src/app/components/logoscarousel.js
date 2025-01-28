54a2d8f2b201d224ee08a774da8027a7e5105768
adsgenda <poseidon_zz@outlook.co.th>
2025-01-28 13:11:51 +0700

    Update logoscarousel.js

--- src/app/components/logoscarousel.js
+++ src/app/components/logoscarousel.js
@@ -3,36 +3,31 @@ import { customerData } from "../data/data";
 import Image from "next/image";
 
 const LogosCarousel = () => {
-  const logosRef = useRef(null);
+  const logoRef = useRef(null);
 
   useEffect(() => {
-    const ul = logosRef.current;
-    ul.insertAdjacentHTML('afterend', ul.outerHTML);
-    ul.nextSibling.setAttribute('aria-hidden', 'true');
+    const ul = logoRef.current;
+    if (ul) {
+      ul.insertAdjacentHTML('afterend', ul.outerHTML);
+      ul.nextSibling.setAttribute('aria-hidden', 'true');
+    }
   }, []);
 
   return (
     <div
-    x-data={''}
-    x-init={$nextTick(() => {
-        let ul = $refs.logos;
-        ul.insertAdjacentHTML('afterend', ul.outerHTML);
-        ul.nextSibling.setAttribute('aria-hidden', 'true');
-    })}
       className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
     >
       <ul
-        x-ref="logo"
+        ref={logoRef}
         className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
       >
-        {customerData.map((item, index)=>{
-            return(
-                <li>
-                <Image src={item.image} key={index} alt={index + 1}/>
-                </li>
-            )
+        {customerData.map((item, index) => {
+          return (
+            <li key={index}>
+              <Image src={item.image} alt={`Logo ${index + 1}`} />
+            </li>
+          );
         })}
-
       </ul>
     </div>
   );
