a105e818bff640c6898090a22f2c0f72c4d48a30
adsgenda <poseidon_zz@outlook.co.th>
2025-03-07 17:31:58 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="light scroll-smooth" dir="ltr">
       <body className={`${noto_sans_thai.variable} font-noto_sans_thai text-base text-black dark:text-white bg-white dark:bg-slate-900`}>
-      <GoogleTagManager gtmId="G-YRQHJZQD4Z" />
+      <GoogleTagManager gtmId="GTM-TLK5743G" />
         <NavLight/>
           {children}
         <Footer/>
