8dd67434331e36bd309ca45e518d4b58fe462f60
adsgenda <poseidon_zz@outlook.co.th>
2025-03-07 17:48:57 +0700

    line edit

--- src/app/components/lineCta.js
+++ src/app/components/lineCta.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import { FaLine  } from '../assets/icons/vander'
 
-export default function LineCta({ href, text = "ติดต่อเรา", fontSize = "text-sm" }) {
+export default function LineCta({ text = "ติดต่อเรา", fontSize = "text-sm" }) {
   return (
     <Link
-      href={href || "https://line.me/R/ti/p/@352tegsj"}
+      href={"https://line.me/R/ti/p/@352tegsj"}
       target="_blank"
       className={`gap-1 h-10 px-6 tracking-wide inline-flex items-center justify-center font-medium rounded-md bg-[#06C755] text-white`} >
         <FaLine className={`${fontSize}`} />
